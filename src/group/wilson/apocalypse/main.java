f2b7de3952d37f8fa9ffca35b8b8355877f83add
707Alex707 <alexandre707@live.com>
2016-11-15 20:36:14 -0500

    Fixed .gitgnore & added runtimes

---
+++ src/group/wilson/apocalypse/main.java
@@ -0,0 +1,33 @@
+package group.wilson.apocalypse;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
+
+/**
+ * Created by Alexandre on 2016-11-15.
+ */
+public class main extends JavaPlugin {
+
+        @Override
+        public void onEnable() {
+            //Fired when the server enables the plugin
+
+            //Reads and writes information from plugin yml to console
+            PluginDescriptionFile pdfFile = getDescription();
+            Logger logger = Logger.getLogger("Minecraft");
+            logger.info(pdfFile.getName() + " plugin has been enabled  (Version " + pdfFile.getVersion() + ")");
+
+        }
+        @Override
+        public void onDisable() {
+            //Fired when the server stops and disables all plugins
+
+            //Reads information from plugin yml and prints version information to console
+            PluginDescriptionFile pdfFile = getDescription();
+            Logger logger = Logger.getLogger("Minecraft");
+            logger.info(pdfFile.getName() + " has been disabled  (Version." + pdfFile.getVersion() + ")");
+        }
+
+
+}
