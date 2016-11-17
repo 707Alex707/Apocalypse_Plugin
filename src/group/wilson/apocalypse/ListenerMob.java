e7991ec575e9c95cb7d873dd01e0f8160eedf797
Spencer F <sfsuper2@gmail.com>
2016-11-17 10:33:31 -0500

    Spencer update

--- src/group/wilson/apocalypse/ListenerMob.java
+++ src/group/wilson/apocalypse/ListenerMob.java
@@ -6,6 +6,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  * Created by Alexandre on 2016-11-16.
  */
@@ -13,12 +17,16 @@ public class ListenerMob implements Listener {
 
     @EventHandler
     //Runs on Mob Death
-    public void mobkill(final EntityDeathEvent event)
-    {
+    public void mobkill(final EntityDeathEvent event) throws FileNotFoundException {
+
+
         //If player kills a monster this fires
         if (event.getEntity() instanceof Monster)
         {
-
+            BufferedReader in = new BufferedReader(new FileReader("kills.txt"));
+            //Need to get the text from the txt first
+            int kills = 0;
+            new Scoreboard(kills);
 
         }
     }
