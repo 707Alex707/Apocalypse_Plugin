5faf43b94ea1f3769d7cac07375290b47cbf2d18
Spencer F <sfsuper2@gmail.com>
2016-11-28 11:17:43 -0500

    Added second reward  -Spencer

--- src/group/wilson/apocalypse/ListenerMob.java
+++ src/group/wilson/apocalypse/ListenerMob.java
@@ -14,6 +14,8 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.FileConfigurationOptions;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.awt.TextComponent;
 import java.io.BufferedReader;
@@ -31,7 +33,7 @@ public class ListenerMob implements Listener {
     //Kills
     public int kills;
     public int Reward1 = 50;
-
+    public int Reward2 = 250;
     main configGetter;
 
     //Passes events to Listener class
@@ -61,6 +63,10 @@ public class ListenerMob implements Listener {
                 player.sendMessage(ChatColor.GOLD + "You have gotten 50 kills! You have recieved 2 extra hearts!");
                 player.setMaxHealth(24);
 
+            }
+            if (Killcount == Reward2){
+                player.sendMessage(ChatColor.GOLD + "You have gotten 250 kills! You have recieved a Perm speed boost and another heart!");
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1), true);
             }
         }
 
