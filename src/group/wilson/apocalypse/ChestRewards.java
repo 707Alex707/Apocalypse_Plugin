c77cf4c3f199855588b0179abd0be5e288638bcc
Spencer F <sfsuper2@gmail.com>
2016-12-01 11:25:38 -0500

    Added loot chest class, will update to give player items FIXED-Spencer

--- src/group/wilson/apocalypse/ChestRewards.java
+++ src/group/wilson/apocalypse/ChestRewards.java
@@ -1,12 +1,18 @@
 package group.wilson.apocalypse;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by s201021621 on 2016-11-30.
@@ -33,6 +39,23 @@ public class ChestRewards implements Listener {
         {
             Player player = (Player)p;
 
+            event.getInventory().getLocation().getBlock().getType().equals(new ItemStack(Material.AIR));
+
+            event.setCancelled(true);
+
+            ItemStack Axe = new ItemStack(Material.DIAMOND_AXE, 1);
+            ItemMeta meta = Axe.getItemMeta();
+            List<String> lores = new ArrayList<String>();
+            lores.add(ChatColor.DARK_PURPLE + "The axe of a long lost survivor");
+
+            meta.setDisplayName(ChatColor.DARK_RED + "Survivor Axe");
+            meta.setLore(lores);
+            Axe.setItemMeta(meta);
+            Axe.setDurability((short) 800);
+            player.getInventory().addItem(Axe);
+
+
+            
             player.sendMessage(ChatColor.GOLD + "You opened a loot chest!");
 
         }
