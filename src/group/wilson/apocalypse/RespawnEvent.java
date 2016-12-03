85e0beaaec97f1decb55c15c887e82524ecc390e
707Alex707 <alexandre707@live.com>
2016-12-03 14:53:10 -0500

    Added Respawn Kit

--- src/group/wilson/apocalypse/RespawnEvent.java
+++ src/group/wilson/apocalypse/RespawnEvent.java
@@ -1,7 +1,5 @@
 package group.wilson.apocalypse;
 
-import net.minecraft.server.v1_10_R1.Item;
-import net.minecraft.server.v1_10_R1.ItemStack;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -11,6 +9,11 @@ import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.FileConfigurationOptions;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Alexandre on 2016-12-02.
@@ -33,8 +36,28 @@ public class RespawnEvent implements Listener {
         //Gets what player died
         final Player p = spawn.getPlayer();
 
-        org.bukkit.inventory.ItemStack is = new org.bukkit.inventory.ItemStack(Material.DIAMOND_SWORD, 1);
-        p.getInventory().addItem(is);
+        /*
+        ItemStack weapon = new ItemStack(Material.DIAMOND_SWORD, 1);
+        ItemStack armor = new ItemStack(Material.IRON_CHESTPLATE);
+        p.getInventory().addItem(weapon);
+
+        ItemStack[] respawnkit;
+
+        List<ItemStack> kit = new ArrayList<ItemStack>();
+        kit.add(new ItemStack(Material.DIAMOND_SWORD, 1));
+        kit.add(new ItemStack(Material.LEATHER_HELMET, 1));
+        kit.add(new ItemStack(Material.LEATHER_CHESTPLATE, 1));
+        kit.add(new ItemStack(Material.LEATHER_LEGGINGS, 1));
+        kit.add(new ItemStack(Material.LEATHER_BOOTS, 1));
+        //p.getInventory().se
+        */
+
+        //Gives player Leather armor and gold chestplate + diamond sword on death
+        p.getInventory().setBoots(new ItemStack(Material.LEATHER_BOOTS, 1));
+        p.getInventory().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS, 1));
+        p.getInventory().setChestplate(new ItemStack(Material.GOLD_CHESTPLATE, 1));
+        p.getInventory().setHelmet(new ItemStack(Material.LEATHER_HELMET, 1));
+        p.getInventory().addItem(new ItemStack(Material.DIAMOND_SWORD, 1));
 
 
 
