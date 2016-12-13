e622bd32eee77928ae4f55cc0509538dcc40be1b
Spencer F <sfsuper2@gmail.com>
2016-12-13 11:17:08 -0500

    Added /chest command with 1 minute cooldown with cooldown message after you try to use it again
    Also added more chest rewards!
    
    Also added join message with kills-Spencer

--- src/group/wilson/apocalypse/PlayerJoin.java
+++ src/group/wilson/apocalypse/PlayerJoin.java
@@ -1,6 +1,7 @@
 package group.wilson.apocalypse;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -11,15 +12,19 @@ import org.bukkit.plugin.Plugin;
  */
 public class PlayerJoin implements Listener {
 
-
-    public PlayerJoin(main plugin) {
-
+    main configGetter;
+    public PlayerJoin(main plugin)
+    {
+        this.configGetter = plugin;
     }
 
     @EventHandler
     public void Join(PlayerJoinEvent join) {
 
+        int Killcount = this.configGetter.getConfig().getInt(join.getPlayer().getName() + " Zombie kills");
+
         join.getPlayer().sendMessage(ChatColor.GOLD + "Welcome to the apocolypse server! Remember to use " + ChatColor.AQUA + "/chest" + ChatColor.GOLD + " to get free items!");
+        join.getPlayer().sendMessage(ChatColor.GOLD + "You currently have " + ChatColor.AQUA + Killcount + ChatColor.GOLD + " Kills!");
 
     }
 }
